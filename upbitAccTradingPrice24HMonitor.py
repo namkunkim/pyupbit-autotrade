95bf15d726f806dc8272cf7b48ce96fe1c632e15
namkunkim <84654357+namkunkim@users.noreply.github.com>
2021-08-15 20:43:02 +0900

    업비트 24시간 거래 대금 탑 10 모니터링 구현 완료

--- upbitAccTradingPrice24HMonitor.py
+++ upbitAccTradingPrice24HMonitor.py
@@ -1,19 +1,11 @@
-import time
-import pyupbit
 import datetime
+import json
+import time
 
+import pyupbit
 import requests
-import schedule
-from fbprophet import Prophet
+
 from logger import Logger
-
-access = "GQ48oKiTje9EdY7ZM7Ek4gbMm9Mi4tmbL9LIr7D7"
-secret = "8yqUpmXMo0mk6SS9NpiXyrnHuizklVrvBWXrrnx0"
-
-
-def regular_report():
-    print(str(datetime.datetime.now()) + "regular_report")
-
 
 def get_acc_trade_price_24h(ticker: str):
     try:
@@ -22,7 +14,7 @@ def get_acc_trade_price_24h(ticker: str):
         headers = {"Accept": "application/json"}
         response = requests.request("GET", url, headers=headers, params=querystring)
         res_json = response.json()
-        print("### " + str(response) + " , " + str(res_json))
+        ##print("### " + str(response) + " , " + str(res_json))
         acc_trade_price_24h = res_json[0]['acc_trade_price_24h']
         return acc_trade_price_24h
     except Exception as ex:
@@ -34,45 +26,111 @@ def update_acc_trade_price_24h_top10():
     acc_trade_price_24h_top10 = []
     acc_trade_price_24h_list = []
     tickers = pyupbit.get_tickers(fiat="KRW")
-    print(tickers)
-    for i in range(12):
-        ticker = tickers.pop()
-        acc_trade_price_24h = get_acc_trade_price_24h(ticker)
-        print(str(ticker) + " : " + str(acc_trade_price_24h))
-        if acc_trade_price_24h is None:
-            logger.error(str(datetime.datetime.now()) + ticker + " is wrong")
-        else:
-            acc_trade_price_24h_list.append([ticker, acc_trade_price_24h])
-        #time.sleep(1)
 
-    print("acc_trade_price_24h_list : " + str(acc_trade_price_24h_list))
+    for ticker in tickers:
+        acc_trade_price_24h = get_acc_trade_price_24h(ticker)
+        ##print(str(ticker) + " : " + str(acc_trade_price_24h))
+        if acc_trade_price_24h is not None:
+            acc_trade_price_24h_list.append([ticker, acc_trade_price_24h])
+        time.sleep(0.1)
+
     acc_trade_price_24h_list.sort(key=lambda r: r[1], reverse=True)
-    print(">>acc_trade_price_24h_list : " + str(acc_trade_price_24h_list))
+
     for i in range(10):
         item = acc_trade_price_24h_list.pop(0)
         acc_trade_price_24h_top10.append(item)
     return acc_trade_price_24h_top10
 
 
-# logger 설정
-logger = Logger()
-logger.info("Starting")
+def get_report(stored, updated):
+    report_msg = "report : "
+    print("store : " + str(stored))
+    print("updated : " + str(updated))
 
-# 로그인
-upbit = pyupbit.Upbit(access, secret)
-acc_trade_price_24h_top10_store = []
+    # 신규 진입 체크
+    for i in range(len(updated)):
+        found = False
+        for j in range(len(stored)):
+            if updated[i][0] == stored[j][0]:
+                found = True
+                break
+        if not found:
+            report_msg += "\n" + str(updated[i][0]) + "is entered in top10"
+            print(str(updated[i][0]) + " is entered in top10")
 
-##매 단위 시간 마다 레포팅
-schedule.every(1).minutes.do(lambda: regular_report)
-# 자동매매 시작
-while True:
-    try:
-        now = datetime.datetime.now()
-        print(str(now) + ":")
-        schedule.run_pending()
-        top10 = update_acc_trade_price_24h_top10()
-        print("top10 : " + str(top10))
-        time.sleep(1)
-    except Exception as e:
-        print(e)
-        time.sleep(1)
+    # 내부 변동 체크 to top level
+    for i in range(len(stored)):
+        for j in range(i + 1, len(updated)):
+            if stored[i][0] == updated[j][0]:
+                report_msg += "\n" + str(stored[i][0]) + " is moved from " + i + " to " + j
+                print(str(stored[i][0]) + " is moved from " + i + " to " + j)
+                continue
+
+    print(report_msg)
+
+    if report_msg == "report : ":
+        return None
+
+    return report_msg
+
+
+def send_report_to_me(report):
+    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+    # 사용자 토큰
+    headers = {
+        "Authorization": "Bearer kakaotalk access token"
+    }
+
+    data = {
+        "template_object": json.dumps({"object_type": "text",
+                                       "text": report,
+                                       "link": {
+                                           "web_url": "www.naver.com"
+                                       }
+                                       })
+    }
+    response = requests.post(url, headers=headers, data=data)
+    print(response.status_code)
+    if response.json().get('result_code') == 0:
+        return '메시지를 성공적으로 보냈습니다.'
+    else:
+        return '메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json())
+
+
+def main():
+    # logger 설정
+    logger = Logger()
+    logger.info("Starting")
+
+    acc_trade_price_24h_top10_store = update_acc_trade_price_24h_top10()
+    while True:
+        try:
+            now = datetime.datetime.now()
+            print(str(now) + ":")
+            top10 = update_acc_trade_price_24h_top10()
+            print("top10 : " + str(top10))
+            report = get_report(acc_trade_price_24h_top10_store, top10)
+            print("report : " + str(report))
+
+            if report is not None:
+                logger.info(str(now) + report)
+                resp = send_report_to_me(report)
+                logger.info(str(now) + " resp for kakaotalk - " + resp)
+            acc_trade_price_24h_top10_store = top10
+            time.sleep(30)
+        except Exception as e:
+            print(e)
+            time.sleep(30)
+
+
+if __name__ == "__main__":
+    main()
+"""top10 = update_acc_trade_price_24h_top10()
+print("top10 : " + str(top10))
+report = get_report(acc_trade_price_24h_top10_store, top10)
+print("report : " + str(report))
+
+if report is not None:
+    logger.info(str(now) + report)
+    resp = send_report_to_me(report)
+    logger.info(str(now) + " resp for kakaotalk - " + resp)"""
